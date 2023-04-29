6e8e79507d1b64227732c2c68d3b625f97174a71
Ahmet Iclier <ahmet.iclier@icloud.com>
2023-04-29 17:30:32 +0200

    fixed json array

--- main.js
+++ main.js
@@ -5,66 +5,64 @@ let dishes = [
         type: "pizza",
         name: "Pizza Margharita",
         ingredients: ["Tomatensauce", "Mozzarella", "Olivenöl"],
-        extras: ["extra Käse", "Rindersalami", "Sucuk", "Oliven"],
         price: 8.90
     },
     {
         type: "pizza",
         name: "Pizza Funghi",
         ingredients: ["Tomatensauce", "Mozzarella", "Olivenöl", "Champignons"],
-        extras: ["extra Käse", "Rindersalami", "Sucuk", "Oliven"],
         price: 9.90
     },
     {
         type: "pizza",
         name: "Pizza Tonno",
         ingredients: ["Tomatensauce", "Mozzarella", "Thunfisch", "Zwiebeln"],
-        extras: ["extra Käse", "Rindersalami", "Sucuk", "Oliven"],
+        
         price: 12.90
     },
     {
         type: "pizzabrötchen",
         name: "Pizzabrötchen gefüllt mit Tomatensauce und Mozzarella",
         ingredients: ["Tomatensauce", "Mozzarella"],
-        extras: ["8 Stück", "12 Stück"],
+        
         price: 13.90
     },
     {
         type: "pizzabrötchen",
         name: "Pizzabrötchen gefüllt mit Hirtenkäse und Sucuk",
         ingredients: ["Hirtenkäse", "Sucuk"],
-        extras: ["8 Stück", "12 Stück"],
+        
         price: 13.90
     },
     {
         type: "pizzabrötchen",
         name: "Pizzabrötchen gefüllt mit Tomatensauce und Mozzarella und Putenschinken",
         ingredients: ["Tomatensauce", "Mozzarella", "Putenschinken"],
-        extras: ["8 Stück", "12 Stück"],
+        
         price: 13.90
     },
     {
         type: "burger",
-        name: "Hamburger Klassik",
-        ingredients: ["180g Rindfleisch-Patty", "Tomate", "Zwiebel", "Gewürzgurke"],
-        extras: ["im Menü mit Pommes Frites und Cola"],
-        price: 8,90
+        name: "Hamburger Classic",
+        ingredients: ["180g Rindfleisch-Patty", "Tomate", "Zwiebel", "Gewürzgurke", "Mayonnaise", "Ketchup"],
+        
+        price: 8.90
     },
     {
         type: "burger",
         name: "Cheeseburger",
-        ingredients: ["180g Rindfleisch-Patty", "Tomate", "Zwiebel", "Gewürzgurke", "Cheddar-Käse", "Ketchup", "Mayonnaise"],
-        extras: ["im Menü mit Pommes Frites und Cola"],
-        price: 9,90
+        ingredients: ["180g Rindfleisch-Patty", "Tomate", "Zwiebel", "Gewürzgurke", "Cheddarkäse", "Mayonnaise", "Ketchup"],
+        
+        price: 9.90
     },
     {
         type: "burger",
-        name: "Ranger Burger",
-        ingredients: ["180g Rindfleisch-Patty", "Tomate", "Zwiebel", "Gewürzgurke", "Cheddar-Käse", "Ketchup", "Mayonnaise", "Rinderschinken"],
-        extras: ["im Menü mit Pommes Frites und Cola"],
-        price: 10,90
+        name: "Sucuk-Burger",
+        ingredients: ["180g Sucuk-Patty", "Tomate", "Zwiebel", "Gewürzgurke", "Cheddarkäse", "Mayonnaise", "Ketchup"],
+        
+        price: 10.90
     },
-]
+];
 
 
 function render() {
