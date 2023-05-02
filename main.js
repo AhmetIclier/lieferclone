671e2218f4019940156b7f4d54c88cd5da057358
Ahmet Iclier <ahmet.iclier@icloud.com>
2023-05-02 13:47:10 +0200

    added plus and minus function

--- main.js
+++ main.js
@@ -64,8 +64,8 @@ let dishes = [
     },
 ];
 let dishname = [];
-let sum = [];
-let amount = [];
+let dishprice = [];
+let dishamount = [];
 
 function render() {
     let dishbox = document.getElementById('renderHere');
@@ -81,88 +81,132 @@ function render() {
                     <div class="icon"><i class="fa-regular fa-plus fa-lg" style="color: #1e1e1e;"></i></div>
                 </div>
                 <div class="dish-info marger">mit ${dishes[i].ingredients} </div>
-                <div class="dish-toppings marger">Wahl aus: extra Käse, Salami, Sucuk</div>
+                
                 <div class="dish-price ">${dishes[i].price.toFixed(2)} €</div>
             </div>
             `
     }
 }
 
+
+// if-abfrage für anzeigen des Warenkorbs bzw. anzeigen des aufrufs für Befüllung des Warenkorbs
+
+
 function addToCart(e) {
     let dish = dishes[e];
-    dishname.push(dish.name);
-    sum.push(dish.price);
-    amount.push(1);
-    console.log(dishname);
-    console.log(sum);
-    console.log(amount);
 
+    
+
+    let dishToAdd = dish.name;
+    if (dishname.includes(dishToAdd)) {
+        let dishIndex = dishname.indexOf(dishToAdd);
+        plusMeal(dishIndex);
+    } else {
+        dishname.push(dish.name);
+        dishprice.push(dish.price);
+        dishamount.push(1);
+    }
+    
+
+    if (dishname.length>0) {
+        let cartbox = document.getElementById('full-cart');
+        cartbox.classList.remove('d-none');
+        let emptycart = document.getElementById('empty-cart');
+        emptycart.classList.add('d-none');
+        let checkout = document.getElementById('checkout');
+        checkout.classList.remove('d-none');
+    }
+
+    renderCheckout();
+}
+
+// function cartSwitcher() {
+
+
+//     if (dishamount.length>0) {
+//         let cartbox = document.getElementById('full-cart');
+//         cartbox.classList.remove('d-none');
+//         let emptycart = document.getElementById('empty-cart');
+//         emptycart.classList.add('d-none');
+//         let checkout = document.getElementById('checkout');
+//         checkout.classList.remove('d-none');
+//     } else if (dishamount.length>)
+// }
+
+// function addCheckout() {
+//     // zwischensumme();
+//     let cartbox = document.getElementById('checkout');
+
+//     cartbox.innerHTML = `
+//     <div class="checkout-line">
+//         <span>Zwischensumme</span>
+//         <span id="zwischensumme">${zwischensumme}</span>
+//     </div>
+//     <div class="checkout-line">
+//         <span>Lieferkosten</span>
+//         <span>1,00 €</span>
+//     </div>
+//     <div class="checkout-line">
+//         <span><b>Gesamt</b></span>
+//         <span id="checkout-summe"></span>
+//     </div>
+//     `;
+
+// }
+
+function renderCheckout() {
     let cartbox = document.getElementById('full-cart');
-    cartbox.classList.remove('d-none');
-    let emptycart = document.getElementById('empty-cart');
-    emptycart.classList.add('d-none');
-    let checkout = document.getElementById('checkout');
-    checkout.classList.remove('d-none');
-
-
-    cartbox.innerHTML += `
-    <div class="dish-in-cart">
-        <table class="cart-table">
-            <tr>
-                <td id="counter${e}">1</td>
-                <td id="identifier${e}">${dish.name}</td>
-                <td class="table-price">${dish.price.toFixed(2)}€</td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><a href="#">Anmerkung hinzufügen</a></td>
-                <td class="edit-amount">
-                    <div class="icon"><i class="fa-solid fa-minus fa-sm" style="color: #1e1e1e;" onclick="minusMeal(${e})"></i></div>
-                    <div class="icon"><i class="fa-regular fa-plus fa-sm" style="color: #1e1e1e;" onclick="plusMeal(${e})"></i></div>
-                </td>
-            </tr>
-        </table>
-    </div>
+    cartbox.innerHTML = '';
+    for (let i = 0; i<dishname.length; i++) {
+        cartbox.innerHTML += `
+            <div class="dish-in-cart" id="dish-num-check${i}">
+                <table class="cart-table">
+                    <tr>
+                        <td id="counter${i}">${dishamount[i]}</td>
+                        <td id="identifier${i}">${dishname[i]}</td>
+                        <td id="dishprice${i}" class="table-price">${dishprice[i].toFixed(2)}€</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><a href="#">Anmerkung hinzufügen</a></td>
+                        <td class="edit-amount">
+                            <div class="icon" onclick="minusMeal(${i})"><i class="fa-solid fa-minus fa-sm" style="color: #1e1e1e;" ></i></div>
+                            <div class="icon" onclick="plusMeal(${i})"><i class="fa-regular fa-plus fa-sm" style="color: #1e1e1e;" ></i></div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
     `;
-
-    addCheckout();
+    }
 }
 
-function addCheckout() {
-    zwischensumme();
-    let cartbox = document.getElementById('checkout');
-
-    cartbox.innerHTML = `
-    <div class="checkout-line">
-        <span>Zwischensumme</span>
-        <span id="zwischensumme">${zwischensumme}</span>
-    </div>
-    <div class="checkout-line">
-        <span>Lieferkosten</span>
-        <span>1,00 €</span>
-    </div>
-    <div class="checkout-line">
-        <span><b>Gesamt</b></span>
-        <span id="checkout-summe"></span>
-    </div>
-    `;
-
+function plusMeal(i){
+    let currentPrice = document.getElementById(`dishprice${i}`);
+    let counter = document.getElementById(`counter${i}`);
+    dishamount[i] +=1;
+    counter.innerHTML = dishamount[i];
+    currentPrice.innerHTML = `${(parseFloat(dishprice[i]) * parseFloat(dishamount[i])).toFixed(2)} €`;
+    
 }
 
-function plusMeal(e){
+function minusMeal(i) {
+    let currentPrice = document.getElementById(`dishprice${i}`);
+    let counter = document.getElementById(`counter${i}`);
+    dishamount[i] -=1;
+    counter.innerHTML = dishamount[i];
+    currentPrice.innerHTML = `${(parseFloat(dishprice[i]) * parseFloat(dishamount[i])).toFixed(2)} €`;
 
+    if(dishamount[i] == 0) {
+        document.getElementById(`dish-num-check${i}`).innerHTML = '';
+        dishname.splice(i, 1);
+        dishprice.splice(i, 1);
+        dishamount.splice(i, 1);
+    }
 }
+
 
 // todo: function plusMeal
 // todo: function minusMeal
 // todo: function check doubles in basket
 // todo: function sum shopping cart
 // todo: function Check mindestbestellwert
-
-let zwischensumme = 0;
-
-function zwischenSumme() {
-    for(let i=0; i<sum.length; i++) {
-        zwischensumme += i;
-    }
-}
